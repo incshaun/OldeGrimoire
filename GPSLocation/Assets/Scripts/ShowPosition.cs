0c1bd66f9dd246be2960a90efc9dc64e2345c483
Shaun B <incshaun@gmail.com>
2019-03-11 13:21:52 +1100

    Added GPS location example

---
+++ GPSLocation/Assets/Scripts/ShowPosition.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowPosition : MonoBehaviour {
+
+        public GPSTracking locationService;
+	
+        public GameObject marker;
+        
+        public float globeRadius = 10.0f;
+        
+        public float rotationSpeed = 30.0f;
+        
+	void Update () {
+          // Rotate the globe.
+          transform.rotation *= Quaternion.AngleAxis (rotationSpeed * Time.deltaTime, Vector3.up);
+		
+          // Plot position.
+          float latitude;
+          float longitude;
+          float altitude;
+          if (locationService.retrieveLocation (out latitude, out longitude, out altitude))
+          {
+            Vector3 position = globeRadius * 
+              new Vector3 (Mathf.Cos (latitude * Mathf.Deg2Rad) * Mathf.Cos (longitude * Mathf.Deg2Rad),
+                           Mathf.Sin (latitude * Mathf.Deg2Rad),
+                           Mathf.Cos (latitude * Mathf.Deg2Rad) * Mathf.Sin (longitude * Mathf.Deg2Rad));
+            marker.transform.localPosition = position;
+          }
+	}
+}
