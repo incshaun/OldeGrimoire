b655dd9a2579eec5a172322a05fd5864e48b4b39
Shaun <incshaun@gmail.com>
2019-03-27 11:02:49 +1100

    Added gesture recogniser prototypes

---
+++ KinectGesture/Assets/Scripts/HandGestures.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Windows.Kinect;
+
+public class HandGestures : MonoBehaviour {
+
+	public BodySourceManager bodyManager;
+
+	void Update () {
+		GetComponent <MeshRenderer> ().material.color = new Color (0, 0, 1);
+		if (bodyManager == null)
+		{
+			return;
+		}
+
+		Body[] data = bodyManager.GetData ();
+		if (data == null)
+		{
+			return;
+		}
+
+		foreach (Body body in data)
+		{
+			if (body == null)
+			{
+				continue;
+			}
+
+			if (body.HandLeftState == Windows.Kinect.HandState.Lasso) {
+				GetComponent <MeshRenderer> ().material.color = new Color (0, 1, 0);
+			}
+		}
+	}
+}
---
+++ KinectGesture/Assets/Scripts/RaiseHand.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Windows.Kinect;
+
+public class RaiseHand : MonoBehaviour {
+
+	public BodySourceManager bodyManager;
+
+	void Update () {
+		GetComponent <MeshRenderer> ().material.color = new Color (0, 0, 1);
+		if (bodyManager == null)
+		{
+			return;
+		}
+
+		Body[] data = bodyManager.GetData ();
+		if (data == null)
+		{
+			return;
+		}
+
+		foreach (Body body in data)
+		{
+			if (body == null)
+			{
+				continue;
+			}
+
+			Windows.Kinect.Joint LHandJoint = body.Joints [JointType.HandLeft];
+			Windows.Kinect.Joint RHandJoint = body.Joints [JointType.HandRight];
+			Windows.Kinect.Joint LShoulderJoint = body.Joints [JointType.ShoulderLeft];
+			Windows.Kinect.Joint RShoulderJoint = body.Joints [JointType.ShoulderRight];
+			if (((LHandJoint.Position.Y > LShoulderJoint.Position.Y) &&
+ 				 (RHandJoint.Position.Y < RShoulderJoint.Position.Y)) ||
+				((LHandJoint.Position.Y < LShoulderJoint.Position.Y) &&
+				 (RHandJoint.Position.Y > RShoulderJoint.Position.Y))) 
+			{
+				GetComponent <MeshRenderer> ().material.color = new Color (0, 1, 0);
+			}
+		}
+	}
+}
