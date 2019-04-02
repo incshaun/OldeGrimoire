1ced7edaf05bd0f5234fbd63bfcb25b452d07c84
Shaun B <incshaun@gmail.com>
2019-04-02 17:23:08 +1100

    Stereo presentation

---
+++ StereoPanorama/Assets/Scripts/Spin.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour {
+  public float speed = 10.0f;
+  
+  void Update () {
+    transform.rotation *= Quaternion.AngleAxis (speed * Time.deltaTime, Vector3.up);
+  }
+}
