1fc708ce4b4adff33460443efe7de1a0859396b1
Shaun B <incshaun@gmail.com>
2019-04-07 14:07:55 +1000

    Cloud anchor example

---
+++ CloudAnchors/Assets/Scripts/AnchorInteraction.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GoogleARCore;
+using GoogleARCore.CrossPlatform;
+using System.IO;
+
+public class AnchorInteraction : MonoBehaviour {
+  
+  public AnchorList anchorList;
+  public Camera firstPersonCamera;
+  public Text updateMessage;
+  
+  void Update () {
+    Touch touch;
+    if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+    {
+      return;
+    }
+    
+    // Screen touched, find a trackable in the image under the touch point.
+    TrackableHit hit;
+    if (Frame.Raycast(touch.position.x, touch.position.y, TrackableHitFlags.PlaneWithinPolygon |TrackableHitFlags.FeaturePointWithSurfaceNormal, out hit))
+    {
+      if (!(hit.Trackable is DetectedPlane) || Vector3.Dot(firstPersonCamera.transform.position - hit.Pose.position, hit.Pose.rotation * Vector3.up) >= 0)
+      {
+        // Hit a point or front of a plane.
+        // Create a local anchor on that trackable.
+        Component anchor = hit.Trackable.CreateAnchor(hit.Pose);
+        if (anchorList != null)
+        {
+          string label = anchorList.getLabel ();
+          // Attach object to the anchor.
+          anchorList.addInstanceToAnchor (label, anchor.transform, hit.Pose.position, hit.Pose.rotation);
+          //Instantiate(markerPrefab, hit.Pose.position, hit.Pose.rotation);
+          
+          // Copy the anchor to the cloud.
+          anchorList.createCloudAnchor (label, (Anchor) anchor);
+        }
+      }
+    }
+  }
+}
---
+++ CloudAnchors/Assets/Scripts/ServerTasks.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class ServerTasks : NetworkBehaviour {
+  private Text monitor;
+  
+  public void Awake ()
+  {
+    monitor = GameObject.Find ("MonitorText").GetComponent <Text> ();
+    monitor.text += "\nStarting ";
+    //NetworkServer.RegisterHandler(MsgType.OnServerInitialized, OnServerInitialized);
+  }
+  public override void OnStartServer()
+  {
+    monitor.text += "\nServer up and running";
+    Debug.Log ("A");
+  }
+  public override void OnStartClient()
+  {
+    base.OnStartClient ();
+    monitor.text += "\nClient running and connected";
+    Debug.Log ("B");
+  }
+}
