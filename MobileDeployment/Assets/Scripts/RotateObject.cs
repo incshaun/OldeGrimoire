c203abefc05068bb335a6ba275f80268df594d2e
Shaun B <incshaun@gmail.com>
2019-05-16 16:01:03 +1000

    First example

---
+++ MobileDeployment/Assets/Scripts/RotateObject.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour {
+	void Update () {
+          transform.rotation *= Quaternion.AngleAxis (0.5f, new Vector3 (0.3f, -0.5f, 0.1f));
+	}
+}
+
