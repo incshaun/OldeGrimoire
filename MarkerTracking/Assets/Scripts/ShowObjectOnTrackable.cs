d6d248fb46b83e70e7fb11f7233af39d54f16cff
Shaun B <incshaun@gmail.com>
2019-05-17 09:26:53 +1000

    AR Core basic tracking

---
+++ MarkerTracking/Assets/Scripts/ShowObjectOnTrackable.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleARCore;
+
+public class ShowObjectOnTrackable : MonoBehaviour {
+  
+  public GameObject displayObject;
+  
+  private List<AugmentedImage> trackedImages = new List<AugmentedImage>();
+  
+  // Update is called once per frame
+  void Update () {
+    displayObject.SetActive (false);
+    
+    Session.GetTrackables<AugmentedImage> (trackedImages, TrackableQueryFilter.All);
+    foreach (var image in trackedImages)
+    {
+      if (image.TrackingState == TrackingState.Tracking)
+      {
+        displayObject.transform.position = image.CenterPose.position;
+        displayObject.transform.rotation = image.CenterPose.rotation;
+        displayObject.SetActive (true);
+      }
+    }
+  }
+}
