9ef50c8f3bf1ac50e934dc191b11ea6de9333c29
Shaun B <incshaun@gmail.com>
2019-05-16 16:19:53 +1000

    unit test example

---
+++ UnitTests/Assets/SceneChangeTests/SceneTest.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneTest {
+
+    [Test]
+    // Make sure the scene change returns an 
+    // error if the name of the destination scene
+    // is invalid.
+    public void SceneChangeChecksDestination() {
+      bool result = SceneChanger.changeScene ("ThisSceneShouldNotExist");
+      Assert.AreEqual (result, false, "Changing to a non existent scene should fail - instead succeeded");
+    }
+
+    [UnityTest]
+    // Make sure that the scene change can actually
+    // change scenes, and end up in a different scene.
+    public IEnumerator SceneChangeResultsInNewScene() {
+      Debug.Log (SceneManager.GetActiveScene ().name + " " + SceneManager.sceneCount + " " + SceneManager.sceneCountInBuildSettings);
+        string targetScene = "TestDestScene";
+      //SceneManager.LoadScene (targetScene);
+        Assert.AreNotEqual (SceneManager.GetActiveScene ().name, targetScene, "Test is starting in the wrong scene.");
+        yield return null;
+        
+        bool result = SceneChanger.changeScene (targetScene);
+        Assert.AreEqual (result, true, "Attempt to change scene to " + targetScene + "failed.");
+
+        // Give a few frames for new scene to load.
+        yield return null;
+        yield return null;
+        yield return null;
+
+        Assert.AreEqual (SceneManager.GetActiveScene ().name, targetScene, "Destination scene not reached after scene change");        
+    }
+}
---
+++ UnitTests/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.IO;
+
+public class SceneChanger : MonoBehaviour {
+
+  // Check if the scene name is in the build settings. Return
+  // the index if so, otherwise return -1.
+  private static int findSceneNameInBuild (string name)
+  {
+    for (int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
+    {
+      if (Path.GetFileNameWithoutExtension (SceneUtility.GetScenePathByBuildIndex (i)).Equals (name))
+      {
+        return i;
+      }
+    }
+    return -1;
+  }
+  
+  // Switch to the scene named. Return true if the operation
+  // succeeds.
+  public static bool changeScene (string destSceneName)
+  {
+    // Checks to see if the destination scene can be invoked.
+    if (findSceneNameInBuild (destSceneName) < 0)
+    {
+      return false;
+    }
+    SceneManager.LoadScene (destSceneName);
+    return true;
+  }
+}
