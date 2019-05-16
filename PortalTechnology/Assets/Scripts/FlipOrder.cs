77a7a19d8ce5c52189ab302e4506e5d35a7384af
Shaun B <incshaun@gmail.com>
2019-05-17 09:50:55 +1000

    Portal example

---
+++ PortalTechnology/Assets/Scripts/FlipOrder.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipOrder : MonoBehaviour {
+  
+  public Material phyCamMaterial;
+  public Material virCamMaterial;
+  public GameObject transition;
+  public GameObject portal;
+  
+  public bool inPhysical = true;
+  
+  // Use this for initialization
+  void Start () {
+    
+  }
+  
+  // Update is called once per frame
+  void Update () {
+    if (inPhysical)
+    {
+      phyCamMaterial.SetInt ("_Stencil_Level", 1);
+      virCamMaterial.SetInt ("_Stencil_Level", 0);
+    }
+    else
+    {
+      phyCamMaterial.SetInt ("_Stencil_Level", 0);
+      virCamMaterial.SetInt ("_Stencil_Level", 1);
+    }
+    
+  }
+  
+  public void OnTriggerEnter(Collider other)
+  {
+    if (other.gameObject == portal)
+    {
+      transition.SetActive (true);
+      Debug.Log ("Changing universe");
+      inPhysical = !inPhysical;
+    }
+  }
+  
+  public void OnTriggerExit (Collider other)
+  {
+    transition.SetActive (false);
+  }
+  
+}
---
+++ PortalTechnology/Assets/Scripts/ShowPhysicalCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+// Show the view from from an image/texture on a plane
+// just in front of the camera. Recommend adding this script to an empty 
+// containing any planes in this category.
+// Set the plane transform to: rot: 90, 180, 0 and scale to 0.1, 0.1, 0.1
+// to achieve a unit size plane facing the z axis. The transform for
+// the empty is set by this script.
+public class ShowPhysicalCamera : MonoBehaviour {
+  
+  public Material camTexMaterial;
+  public Camera camera;
+  
+  private WebCamTexture webcamTexture;
+  
+  void Start () {
+    webcamTexture = new WebCamTexture ();
+    
+    camTexMaterial.mainTexture = webcamTexture;
+    webcamTexture.Play ();
+  }
+  
+  void Update ()
+  {
+    // With thanks: https://answers.unity.com/questions/314049/how-to-make-a-plane-fill-the-field-of-view.html
+    float pos = (camera.nearClipPlane + 0.01f);
+    
+    transform.position = camera.transform.position + camera.transform.forward * pos;
+    
+    float h = Mathf.Tan (camera.fieldOfView * Mathf.Deg2Rad * 0.5f) * pos * 2.0f;
+    
+    transform.localScale = new Vector3(h * camera.aspect, h, 1.0f);
+  }
+}
