89a615d65bb8997f36983f21654075f669a2448e
Shaun B <incshaun@gmail.com>
2020-01-31 16:19:55 +1100

    Intro quest example

---
+++ MobileHeadset/Assets/Scripts/MoveInDirectionOf.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveInDirectionOf : MonoBehaviour
+{
+    [Tooltip ("Controller input used to trigger movement")]
+    public OVRInput.RawButton button = OVRInput.RawButton.LThumbstickUp;
+  
+    [Tooltip ("Speed of movement.")]
+    public float speed = 10.0f;
+  
+    [Tooltip ("Object used to determine direction of travel")]
+    public GameObject pointer;
+    
+    [Tooltip ("Constraint vector used to filter axes of movement. The normal to the plane that movement is projected to.")]
+    public Vector3 constrainDirection = new Vector3 (0, 1, 0);
+    
+    void Update()
+    {
+      if (OVRInput.Get (button))
+      {
+        this.transform.position += speed * Vector3.ProjectOnPlane (pointer.transform.forward, constrainDirection);
+      }
+    }
+}
