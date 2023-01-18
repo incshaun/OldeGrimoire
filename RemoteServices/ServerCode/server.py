a7c1e08dcbd797d6a4acff8b2f704a3be581602e
incshaun <incshaun@gmail.com>
2023-01-18 15:47:10 +1100

    Improvements to the server with speech recognition stub. Will require a whisper installation script.

--- RemoteServices/ServerCode/server.py
+++ RemoteServices/ServerCode/server.py
@@ -1,4 +1,6 @@
 import socket
+import time
+import wave
 
 serverPort = 8800
 maxClients = 2
@@ -21,14 +23,57 @@ def server ():
   finally:
       sock.close(  )
 
+def saveToWav (filename, data):
+  fl = wave.open (filename, 'wb')
+  fl.setnchannels(1) 
+  fl.setsampwidth(2)
+  fl.setframerate(16000)
+  fl.writeframesraw (data)
+  fl.close()
+
+import whisper
+
+speechModel = None
+def invokeSpeechRecognition ():
+  global speechModel
+  
+  if speechModel == None:
+    speechModel = whisper.load_model("base")
+  result = speechModel.transcribe("my.wav")
+  print(result["text"])
+  return result["text"]
+
+def readAmount (clientSocket, amount):
+  buf = bytearray ()
+  while len (buf) < amount:
+    readAmount = clientSocket.recv (amount - len (buf))
+    buf += readAmount
+    print ("Buf len: ", len (buf), amount - len (buf))
+  return buf
+
+def readInt (clientSocket):
+  buf = readAmount (clientSocket, 4)
+  print ("Read", buf, int.from_bytes (buf, byteorder='big', signed=False))
+  return int.from_bytes (buf, byteorder='big', signed=False)
+
 def handleConnection (clientSocket, address):    
 
   while True:
-      receivedData = clientSocket.recv (blockSize)
+      size = readInt (clientSocket)
+      receivedData = readAmount (clientSocket, size)
+      print ("Received: ", len (receivedData))
       if not receivedData: 
         break
-      clientSocket.send (receivedData)
+      
+      saveToWav ("my.wav", receivedData)
+      #time.sleep (5)
+      
+      result = str.encode (invokeSpeechRecognition ())
+      
+      clientSocket.send (result)
+      print ("Sent reply")
   clientSocket.close(  )
   print ("Disconnected:", address)
 
 server ()
+#invokeSpeechRecognition ()
