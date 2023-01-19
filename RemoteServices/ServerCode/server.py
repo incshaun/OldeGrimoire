44083bd66e696aef8e67d4ecd261945571720535
incshaun <incshaun@gmail.com>
2023-01-19 12:24:26 +1100

    More resilient server structure to handle connections coming and going.

--- RemoteServices/ServerCode/server.py
+++ RemoteServices/ServerCode/server.py
@@ -1,25 +1,42 @@
 import socket
 import time
 import wave
+import signal
+import sys
+import threading
 
 serverPort = 8800
 maxClients = 2
 blockSize = 512
 
+activeSockets = []
+
+def signal_handler(signal, frame):
+  for sock in activeSockets:
+    sock.close ()
+  print ("Socket closed ", activeSockets)
+  sys.exit (0)
+
 def server ():
-  
   sock = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
   sock.setsockopt (socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
   sock.bind (('', serverPort))
 
   sock.listen (maxClients)
-
+  
+  activeSockets.append (sock)
+  signal.signal(signal.SIGINT, signal_handler)
+  
   try:
       while True:
           clientSocket, address = sock.accept ()
           print ("Connection from: ", address)
-          handleConnection (clientSocket, address)
+          activeSockets.append (clientSocket)
+          
+          serviceThread = threading.Thread (target = handleConnection, args = (clientSocket, address), daemon = True)
+          serviceThread.start ()
+          #handleConnection (clientSocket, address)
   finally:
       sock.close(  )
 
@@ -40,40 +57,64 @@ def invokeSpeechRecognition ():
   if speechModel == None:
     speechModel = whisper.load_model("base")
   result = speechModel.transcribe("my.wav")
-  print(result["text"])
+  #print(result["text"])
   return result["text"]
 
 def readAmount (clientSocket, amount):
   buf = bytearray ()
   while len (buf) < amount:
-    readAmount = clientSocket.recv (amount - len (buf))
-    buf += readAmount
-    print ("Buf len: ", len (buf), amount - len (buf))
+    try:
+      readAmount = clientSocket.recv (amount - len (buf))
+      if not readAmount: # socket probably closed.
+        raise IOError
+      
+      buf += readAmount
+    except Exception as e:
+      print ("Error reading on socket")
+      raise e
+  #print ("Buf len: ", len (buf), amount - len (buf))
   return buf
 
+def writeAmount (clientSocket, data):
+  clientSocket.send (data)
+
 def readInt (clientSocket):
-  buf = readAmount (clientSocket, 4)
-  print ("Read", buf, int.from_bytes (buf, byteorder='big', signed=False))
-  return int.from_bytes (buf, byteorder='big', signed=False)
+  try:
+    buf = readAmount (clientSocket, 4)
+  #  print ("Read", buf, int.from_bytes (buf, byteorder='big', signed=False))
+    return int.from_bytes (buf, byteorder='big', signed=False)
+  except Exception as e:
+    raise e
+
+def writeInt (clientSocket, v):
+  clientSocket.send (v.to_bytes (4, byteorder='big', signed=False))
 
 def handleConnection (clientSocket, address):    
 
-  while True:
-      size = readInt (clientSocket)
-      receivedData = readAmount (clientSocket, size)
-      print ("Received: ", len (receivedData))
-      if not receivedData: 
-        break
-      
-      saveToWav ("my.wav", receivedData)
-      #time.sleep (5)
-      
-      result = str.encode (invokeSpeechRecognition ())
-      
-      clientSocket.send (result)
-      print ("Sent reply")
-  clientSocket.close(  )
-  print ("Disconnected:", address)
+  try:
+    while True:
+        size = readInt (clientSocket)
+        receivedData = readAmount (clientSocket, size)
+  #      print ("Received: ", len (receivedData))
+        if not receivedData: 
+          break
+        
+        saveToWav ("my.wav", receivedData)
+        #time.sleep (5)
+        
+        result = str.encode (invokeSpeechRecognition ())
+        
+        writeInt (clientSocket, len (result))
+        writeAmount (clientSocket, result)
+        print ("Sent reply", result)
+    clientSocket.close(  )
+    print ("Disconnected:", address, len (activeSockets))
+    activeSockets.remove (clientSocket)
+  except Exception as e:
+    activeSockets.remove (clientSocket)
+    clientSocket.close(  )
+    print ("Service exception: ", str (e))
+    return
 
 server ()
 #invokeSpeechRecognition ()
