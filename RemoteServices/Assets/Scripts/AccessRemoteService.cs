2bfa1333521f5a4ee69889d7a70c19ace61ebfee
incshaun <incshaun@gmail.com>
2023-01-20 08:45:05 +1100

    Replaced synchronization with more robust solution that will scale to additional services.

--- RemoteServices/Assets/Scripts/AccessRemoteService.cs
+++ RemoteServices/Assets/Scripts/AccessRemoteService.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Net.Sockets;
 
 using TMPro;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Linq;
 
@@ -17,9 +18,33 @@ public class AccessRemoteService : MonoBehaviour
     
     public string serverName = "192.168.1.150";
     public int serverPort = 8800;
+
+    private class ServiceConnection
+    {
+      public TcpClient client = null;
+      public SemaphoreSlim connectionMutex = null;  
+      // This field is cosmetic only, to indicate when the server backlog starts to grow.
+      public bool locked = false;
+      
+      public void shutdown ()
+      {
+        if (client != null)
+        {
+            client.Close();
+            client = null;
+        }
+      }
+      
+      public ServiceConnection ()
+      {
+          client = null;
+          connectionMutex = new SemaphoreSlim (1);
+          locked = false;
+      }
+    }
     
     //private int blockSize = 1024;
-    private TcpClient client = null;
+//     private TcpClient client = null;
 
     public static byte[] networkShortToByte(short s)
     {
@@ -64,7 +89,7 @@ public class AccessRemoteService : MonoBehaviour
             }
             return buffer;
         }
-        catch (Exception e)
+        catch (Exception)
         {
             Debug.Log("Error reading data");
             return null;
@@ -86,56 +111,70 @@ public class AccessRemoteService : MonoBehaviour
 
     void OnApplicationQuit()
     {
-        if (client != null)
+        if (speechService != null)
         {
-            client.Close();
+            speechService.shutdown ();
         }
     }
 
-    private async Task<byte []> sendAndReceive (byte [] data)
+    private async Task<byte []> sendAndReceive (ServiceConnection service, byte [] data)
     {
+        // Messages on a single service are queued, so multiple threads sending a request
+        // and waiting for a response have to do this sequentially. Separate services have
+        // their own connections so can run in parallel but increase server overhead.
+        // Mutex lock at the start and end ensure this. Any early leaving must unlock the
+        // mutex.
+        
+        if (service.locked)
+        {
+            Debug.Log ("Warning. Will have to block on sendAndReceive. Potential server overload situation.");
+        }
+        await service.connectionMutex.WaitAsync ();
+        service.locked = true;
+        
         byte[] result = null;
 
         try
         {
-            if (client == null)
+            if (service.client == null)
             {
                 IPAddress[] addresslist = await Dns.GetHostAddressesAsync(serverName);
                 //Debug.Log("Addresses: " + addresslist);
                 try
                 {
-                    client = new TcpClient();
-                    await client.ConnectAsync(addresslist[0], serverPort);
+                    service.client = new TcpClient();
+                    await service.client.ConnectAsync(addresslist[0], serverPort);
                     Debug.Log("Connected");
                 }
-                catch (SocketException e)
+                catch (SocketException)
                 {
                     Debug.Log("Could not connect: check that the server is actually running.");
-                    client = null;
+                    service.client = null;
                 }
             }
 
-            if ((client != null) && (client.Connected))
+            if ((service.client != null) && (service.client.Connected))
             {
                 //Debug.Log("Sending");
                 NetworkStream stream = null;
                 try
                 {
-                    stream = client.GetStream();
+                    stream = service.client.GetStream();
                 }
-                catch (InvalidOperationException e)
+                catch (InvalidOperationException)
                 {
                     Debug.Log("Could not get stream: check that the server is actually running.");
                     stream = null;
-                    client = null;
+                    service.client = null;
                 }
 
                 if (stream != null)
                 {
                     // Send a header with the length of the next block in the stream.
-                    await stream.WriteAsync(networkUIntToByte((uint)data.Length));
+                    byte [] bint = networkUIntToByte((uint)data.Length);
+                    await stream.WriteAsync(bint, 0, bint.Length);
 
-                    await stream.WriteAsync(data);
+                    await stream.WriteAsync(data, 0, data.Length);
                     //Debug.Log("Sent");
                     int length = await readInt(stream);
 //                     Debug.Log ("Expect: " + length);
@@ -148,10 +187,13 @@ public class AccessRemoteService : MonoBehaviour
         catch (Exception e)
         {
             Debug.Log("Exception in sendAndReceive: " + e);
-            client = null;
-            return null;
+            service.client = null;
+            result = null;
         }
 
+        service.locked = false;
+        service.connectionMutex.Release ();
+        
         return result;
     }
 
@@ -212,16 +254,16 @@ public class AccessRemoteService : MonoBehaviour
     {
         Debug.Log("Transmitting data: " + inputTextField.text);
 
-        // The actual interactions with the remote server will use byte arrays,
-        // to allow any form of data to interchanged. This method converts to/from
-        // text for demonstration/testing purposes.
-        byte[] data = Encoding.ASCII.GetBytes(inputTextField.text);
-        byte[] result = await sendAndReceive(data);
-
-        string resultString = Encoding.ASCII.GetString(result);
-        outputTextField.text = resultString;
-
-        Debug.Log("Received result: " + outputTextField.text);
+//         // The actual interactions with the remote server will use byte arrays,
+//         // to allow any form of data to interchanged. This method converts to/from
+//         // text for demonstration/testing purposes.
+//         byte[] data = Encoding.ASCII.GetBytes(inputTextField.text);
+//         byte[] result = await sendAndReceive(data);
+// 
+//         string resultString = Encoding.ASCII.GetString(result);
+//         outputTextField.text = resultString;
+// 
+//         Debug.Log("Received result: " + outputTextField.text);
     }
 
     //private async Task doTransmitAudio ()
@@ -269,8 +311,8 @@ public class AccessRemoteService : MonoBehaviour
     private bool audioRecording = false;
     private int lastRecordPosition;
     private int recordBufferLength;
-    private int recordDuration = 2; // seconds. Should be longer than the transmissionDuration. Any extra adds to latency, but improves resiliance to delays.
-    private int transmissionDuration = 1; // seconds.
+    private int recordDuration = 10; // seconds. Should be longer than the transmissionDuration. Any extra adds to latency, but improves resiliance to delays.
+    private int transmissionDuration = 5; // seconds.
     private int recordRate = 16000; // samples per second.
     private int transmissionThreshold;
     private void doTransmitAudioStream()
@@ -283,18 +325,18 @@ public class AccessRemoteService : MonoBehaviour
         audioRecording = true;
     }
 
-    // Since requests to the server are sequentially over a single stream,
-    // we have to wait until the current request is complete before sending
-    // the next, or the stream will get messed up with interleaved requests.
-    private Task runningTask = null;
-    private async Task transmitAsRequired ()
+//     // Since requests to the server are sequentially over a single stream,
+//     // we have to wait until the current request is complete before sending
+//     // the next, or the stream will get messed up with interleaved requests.
+//     private Task runningTask = null;
+    private void transmitAsRequired (ServiceConnection speechService)
     {
-        if (runningTask != null)
-        {
-            await Task.WhenAny (runningTask);
-            runningTask = null;
-        }
-        
+//         if (runningTask != null)
+//         {
+//             await Task.WhenAny (runningTask);
+//             runningTask = null;
+//         }
+//         
         try
         {
             //Debug.Log("Recording at position: " + Microphone.GetPosition(Microphone.devices[0]));
@@ -325,7 +367,7 @@ public class AccessRemoteService : MonoBehaviour
                 // The actual interactions with the remote server will use byte arrays,
                 // to allow any form of data to interchanged. This method converts to/from
                 // text for demonstration/testing purposes.
-                runningTask = sendAndUpdate(data);
+                _ = sendAndUpdate(speechService, data);
             }
         }
         catch (Exception e)
@@ -334,16 +376,22 @@ public class AccessRemoteService : MonoBehaviour
         }
     }
 
+    private ServiceConnection speechService = null; // run a single connection for speech services.
     public void speechRecognitionService()
     {
+        if (speechService == null)
+        {
+            speechService = new ServiceConnection ();
+        }
+        
         doTransmitAudioStream();
     }
 
-    private async Task sendAndUpdate(byte[] data)
+    private async Task sendAndUpdate(ServiceConnection service, byte [] data)
     {
         try
         {
-            byte[] result = await sendAndReceive(data);
+            byte[] result = await sendAndReceive(service, data);
 
             if ((result != null) && (result.Length > 0))
             {
@@ -364,7 +412,7 @@ public class AccessRemoteService : MonoBehaviour
         //Debug.Log("Main thread running");
         if (audioRecording)
         {
-            _ = transmitAsRequired();
+            transmitAsRequired(speechService);
         }
     }
 }
