bdc2a56cb41e00e520f65ada13d1c92fee22e0f6
incshaun <incshaun@gmail.com>
2025-01-07 08:50:00 +1100

    Updated component to Unity 6

--- MobileDeployment/Assets/Scripts/PhysicalCameraTexture.cs
+++ MobileDeployment/Assets/Scripts/PhysicalCameraTexture.cs
@@ -1,43 +1,56 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+#if UNITY_ANDROID
+using UnityEngine.Android;
+#endif
+using TMPro;
 
 public class PhysicalCameraTexture : MonoBehaviour {
-
-        public Material camTexMaterial;
-        
-        public Text outputText;
-        
-        private WebCamTexture webcamTexture;
-        
-        private int currentCamera = 0;
-        
-        private void showCameras ()
-        {
-          outputText.text = "";
-          foreach (WebCamDevice d in WebCamTexture.devices)
-          {
-            outputText.text += d.name + (d.name == webcamTexture.deviceName ? "*" : "") + "\n";
-          }
-        }
-        
-        public void nextCamera ()
-        {
-          currentCamera = (currentCamera + 1) % WebCamTexture.devices.Length;
-          // Change camera only works if the camera is stopped.
-          webcamTexture.Stop ();
-          webcamTexture.deviceName = WebCamTexture.devices[currentCamera].name;
-          webcamTexture.Play ();
-          showCameras ();
-        }
-        
-	void Start () {
-	  webcamTexture = new WebCamTexture ();
-
-          showCameras ();
-          
-          camTexMaterial.mainTexture = webcamTexture;
-          webcamTexture.Play ();
-	}
+  
+  public Material camTexMaterial;
+  
+  private WebCamTexture webcamTexture;
+  
+  public TextMeshProUGUI outputText;
+  
+  private int currentCamera = 0;
+  
+  private void showCameras ()
+  {
+    outputText.text = "";
+    foreach (WebCamDevice d in WebCamTexture.devices)
+    {
+      outputText.text += d.name + (d.name == webcamTexture?.deviceName ? "*" : "") + "\n";
+    }
+  }
+  
+  public void nextCamera ()
+  {
+    currentCamera = (currentCamera + 1) % WebCamTexture.devices.Length;
+    // Change camera only works if the camera is stopped.
+    webcamTexture.Stop ();
+    webcamTexture.deviceName = WebCamTexture.devices[currentCamera].name;
+    webcamTexture.Play ();
+    showCameras ();
+  }
+  
+  void Update () {
+    showCameras ();
+    if (webcamTexture == null)
+    {
+      webcamTexture = new WebCamTexture ();
+      #if UNITY_ANDROID         
+      if (!Permission.HasUserAuthorizedPermission(Permission.Camera))
+      {
+        webcamTexture = null;
+      }
+      #endif              
+    }
+    if (!webcamTexture.isPlaying)
+    {
+      camTexMaterial.mainTexture = webcamTexture;
+      webcamTexture.Play ();
+    }
+  }
 }
--- MobileDeployment/Assets/Scripts/RotateObject.cs
+++ MobileDeployment/Assets/Scripts/RotateObject.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class RotateObject : MonoBehaviour {
-	void Update () {
-          transform.rotation *= Quaternion.AngleAxis (0.5f, new Vector3 (0.3f, -0.5f, 0.1f));
-	}
+  void Update () {
+    transform.rotation *= Quaternion.AngleAxis (0.5f, new Vector3 (0.3f, -0.5f, 0.1f));
+  }
 }
-
