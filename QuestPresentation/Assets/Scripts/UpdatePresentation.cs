568146e89d9551f29ab1e4b83ce9cdba48318339
incshaun <incshaun@gmail.com>
2025-01-09 11:17:58 +1100

    Initial draft of a basic headset based interaction component.

---
+++ QuestPresentation/Assets/Scripts/UpdatePresentation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdatePresentation : MonoBehaviour {
+
+  public List<Texture> slides;
+  
+  private int currentSlide;
+  
+  private void updateScreen ()
+  {
+    GetComponent <MeshRenderer> ().material.mainTexture = slides[currentSlide];
+  }
+  
+  public void Start ()
+  {
+    currentSlide = 0;
+    updateScreen ();
+  }
+  
+  public void nextSlide ()
+  {
+    currentSlide = (currentSlide + slides.Count + 1) % slides.Count;
+    updateScreen ();
+  }
+
+  public void previousSlide ()
+  {
+    currentSlide = (currentSlide + slides.Count - 1) % slides.Count;
+    updateScreen ();
+  }
+}
